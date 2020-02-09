ecfd94a92460ad35873bf084dd2f4541ceae044e
putudoni <putuwigadoni@yahoo.com>
2020-02-09 18:54:57 +0700

    Setup project

---
+++ src/main/java/com/pnwd/srca/SimpleRestfulCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.pnwd.srca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestfulCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestfulCrudApiApplication.class, args);
+	}
+
+}
