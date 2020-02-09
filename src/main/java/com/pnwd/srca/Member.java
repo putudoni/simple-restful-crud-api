2ae7937980fcbbcea4cdfea615ff31464bec2b8e
putudoni <putuwigadoni@yahoo.com>
2020-02-09 19:04:29 +0700

    Create Member class

---
+++ src/main/java/com/pnwd/srca/Member.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package com.pnwd.srca;
+
+import java.util.Date;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ * @version 1.0
+ */
+public class Member {
+
+	private Long id;
+	
+	private String name;
+	
+	private String gender;
+	
+	private String email;
+	
+	private String address;
+	
+	private Date createdDate;
+	
+	private Date modifiedDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+}
