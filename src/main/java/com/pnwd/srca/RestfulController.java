aeed5e5f424ac6117add4cd5a6f7cbd26714c4fd
putudoni <putuwigadoni@yahoo.com>
2020-02-09 18:57:30 +0700

    Create RestfulController class

---
+++ src/main/java/com/pnwd/srca/RestfulController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.pnwd.srca;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class RestfulController {
+
+}
