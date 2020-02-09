d3ee22f3e2eb628e50d08b8c858321c18a9880f1
putudoni <putuwigadoni@yahoo.com>
2020-02-09 19:15:45 +0700

    Create findAll method

--- src/main/java/com/pnwd/srca/MemberService.java
+++ src/main/java/com/pnwd/srca/MemberService.java
@@ -3,9 +3,12 @@
  */
 package com.pnwd.srca;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Putu Ngurah Wigadoni
@@ -16,5 +19,10 @@ public class MemberService {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	@Transactional(readOnly=true)
+	public List<Member> findAll(){
+		return jdbcTemplate.query("SELECT * FROM member",new MemberRowMapper());
+	}
 	
 }
