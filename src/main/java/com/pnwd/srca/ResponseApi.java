d11747b5fd7fda58a5d712d42433d60e6c8687a8
putudoni <putuwigadoni@yahoo.com>
2020-02-09 19:16:47 +0700

    Create ResponseApi class

---
+++ src/main/java/com/pnwd/srca/ResponseApi.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.pnwd.srca;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ * @version 1.0
+ */
+public class ResponseApi {
+
+	private String status;
+
+	private Object data;
+
+	private String messages;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public String getMessages() {
+		return messages;
+	}
+
+	public void setMessages(String messages) {
+		this.messages = messages;
+	}
+
+}
