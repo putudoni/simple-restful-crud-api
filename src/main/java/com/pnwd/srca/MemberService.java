575c2be240ac3c8ffd89cb77a9c68cd6f4de74ea
putudoni <putuwigadoni@yahoo.com>
2020-02-09 19:05:39 +0700

    Create MemberService class

---
+++ src/main/java/com/pnwd/srca/MemberService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.pnwd.srca;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ * @version 1.0
+ */
+@Service
+public class MemberService {
+
+}
