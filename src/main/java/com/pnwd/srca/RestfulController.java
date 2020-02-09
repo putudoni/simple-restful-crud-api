c7e155ac54202ddcc386361b87ef816a6bc7ceaa
putudoni <putuwigadoni@yahoo.com>
2020-02-09 19:19:57 +0700

    Create findAllMethod in RestfulController

--- src/main/java/com/pnwd/srca/RestfulController.java
+++ src/main/java/com/pnwd/srca/RestfulController.java
@@ -3,6 +3,10 @@
  */
 package com.pnwd.srca;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class RestfulController {
 
+	@Autowired
+	private MemberService memberService;
+	
+	@GetMapping("/member")
+	public ResponseEntity<ResponseApi> findAllMember(){
+		ResponseApi response = new ResponseApi();
+		response.setStatus(HttpStatus.OK.toString());
+		response.setData(memberService.findAll());
+		return new ResponseEntity<>(response,HttpStatus.OK);
+	}
+	
 }
