8c43866092b40934386287c734d9bd7a37e79b23
putuwigadoni <putuwigadoni@yahoo.com>
2022-04-21 13:41:35 +0700

    Use @Data annotation in Account Entity

--- src/main/java/com/pnwd/srca/Member.java
+++ src/main/java/com/pnwd/srca/Member.java
@@ -5,10 +5,13 @@ package com.pnwd.srca;
 
 import java.util.Date;
 
+import lombok.Data;
+
 /**
  * @author Putu Ngurah Wigadoni
  * @version 1.0
  */
+@Data
 public class Member {
 
 	private Long id;
@@ -25,60 +28,4 @@ public class Member {
 	
 	private Date modifiedDate;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
-
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
-	
 }
